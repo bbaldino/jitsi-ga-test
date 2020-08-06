e0e0583d848d0e4fc5bc072572d4276d61c800fb
Brian Baldino <bbaldino@gmail.com>
2020-08-06 16:56:20 -0700

    update

--- entrypoint.py
+++ entrypoint.py
@@ -4,10 +4,25 @@ import json
 import os
 import sys
 
+def error(msg: str) -> None:
+    print(f"[error]: {msg}"
+
+def info(msg: str) -> None:
+    print(f"[info]: {msg}")
+
 if __name__ == "__main__":
     GITHUB_EVENT_PATH = os.environ["GITHUB_EVENT_PATH"]
 
     with open(GITHUB_EVENT_PATH) as event_info_file:
         event = json.load(event_info_file)
 
-    print("loaded event info: %s" % event)
+    info("loaded event info: %s" % event)
+    pr_comment = event.get("issue", default={}).get("pull_request", default=None)
+    if pr_comment == None:
+        info("Event is not a PR comment")
+        # TODO: this should just be a 'quit', not a fail or success.  Is this right?
+        sys.exit(0)
+
+    comment_body = event.get("comment").get("body")
+    info(f"Parsed comment body {comment_body}")
+
