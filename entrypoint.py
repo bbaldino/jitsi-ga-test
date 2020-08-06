39886ef88a6828a7307e22bd44f613e0a46aab06
Brian Baldino <bbaldino@gmail.com>
2020-08-06 16:40:56 -0700

    python entry

---
+++ entrypoint.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import json
+
+if __name__ == "__main__":
+    GITHUB_EVENT_PATH = os.environ["GITHUB_EVENT_PATH"]
+
+    with open(GITHUB_EVENT_PATH) as event_info_file:
+        event = json.load(event_info_file)
+
+    print("loaded event info: %s" % event)
