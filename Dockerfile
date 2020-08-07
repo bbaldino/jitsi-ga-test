995e51637f2afa8bf9325c25ccdd4cc353b3f494
Brian Baldino <bbaldino@gmail.com>
2020-08-07 10:51:45 -0700

    update

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ FROM python:3-alpine
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.py .
+COPY requirements.txt .
+RUN pip3 install --no-cache-dir -r requirements.txt
 
 # Code file to execute when the docker container starts up (`entrypoint.sh`)
 ENTRYPOINT ["/entrypoint.py"]
--- entrypoint.py
+++ entrypoint.py
@@ -4,11 +4,23 @@ import json
 import os
 import sys
 
+import git
+
 def error(msg: str) -> None:
     print(f"[error]: {msg}")
 
 def info(msg: str) -> None:
     print(f"[info]: {msg}")
+
+# check these out to 'overrides/'
+def checkout_component(component_name, repo, branch_name, checkout_dir):
+    git.Repo.clone_from(f"https://github.com/{repo}.git", os.path.join(checkout_dir, component_name), branch=branch_name, depth=1)
+
+def checkout_overridden_components(overridden_components):
+    os.mkdir("component_overrides")
+    for (component, (repo, branch)) in overridden_components.items():
+        print(f"Will use branch {branch} from repo {repo} for component {component}")
+        checkout_component(component, repo, branch, "component_overrides")
 
 if __name__ == "__main__":
     GITHUB_EVENT_PATH = os.environ["GITHUB_EVENT_PATH"]
@@ -24,5 +36,37 @@ if __name__ == "__main__":
         sys.exit(0)
 
     comment_body = event.get("comment").get("body")
-    info(f"Parsed comment body {comment_body}")
+    info(f"Parsed comment body '{comment_body}'")
+    # TEMP - hard code comment body to test
+    comment_body = """deps:
+    use jitsi-videobridge bbaldino/jitsi-videobridge jetty_activator
+    """
+    # END TEMP
+
+    if not comment_body.startswith("deps"):
+        info("Not a deps comment, ignoring")
+        sys.exit(0)
+
+    overridden_components = dict()
+    # Separate each line, ignoring the first one ('deps:')
+    lines = [line.strip() for line in comment_body.split("\n")[1:]]
+    for line in lines:
+        try:
+            print(line)
+            print(line.split(" "))
+            # The expected format is "use <component name> <repo> <branch>
+            # Where:
+            # component name is something like 'jitsi-videobridge' or 'jicofo'
+            # repo is owner/repo name, e.g.: 'bbaldino/jitsi-videobridge'
+            # branch is the branch name, e.g.: my_cool_feature
+            (_, component, repo, branch) = line.split(" ")
+            info(f"Will use branch {branch} from repo {repo} for component {component}")
+            overridden_components[component] = (repo, branch)
+        except ValueError as err:
+            info(f"invalid line: {err}")
+
+    checkout_overridden_components(overridden_components)
+    for root, dirs, files in os.walk("./component_overrides"):
+        for filename in files:
+            print(filename)
 
